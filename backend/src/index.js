1920d5483bde5855ef72fb4805b720000370b151
maheshray1221 <mr5832388@gmail.com>
2025-10-24 11:51:57 +0530

    start code

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: "./.env" })
+
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+connectDB()
+    .then(() => {
+        const port = process.env.PORT || 3000
+        app.listen(port, () => {
+            console.log(`continue working on port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log("Error while listen time", err)
+    })
